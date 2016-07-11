0e5fc47a9e666193fed000a989af8ddd504e6dbc
damiano-migme <damiano.t@mig.me>
2016-07-11 12:58:57 +0800

    Add gem strptime for performance improvement

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 # start-cron.sh
 fluent-gem install --no-document fluent-plugin-secure-forward
 fluent-gem install --no-document fluent-plugin-elasticsearch
+fluent-gem install --no-document strptime
 fluentd -c /fluentd/etc/$FLUENTD_CONF
